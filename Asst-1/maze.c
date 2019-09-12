a87cce1d9698341c2a74d7ff9dbd4099dc0cd8a8
Xiaoxiao He <41120957+hexiaoxiao-cs@users.noreply.github.com>
2019-09-11 23:09:45 -0400

    Initialize Maze Project

---
+++ Asst-1/maze.c
@@ -0,0 +1,12 @@
+//
+// Created by Xiaoxiao He on 9/11/2019.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int** maze; //storage for maze information
+
+int main(int argc, char* argv[]){
+    int
+}
