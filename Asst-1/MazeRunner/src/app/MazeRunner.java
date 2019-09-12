83db15a3e3657eb69e8e5acf275555af4d1f005c
sarahliang01 <46304579+sarahliang01@users.noreply.github.com>
2019-09-12 00:50:46 -0400

    created eclipse project.
    It doesnt do anything yet but you can import the project into eclipse to run the code (or lack thereof )
    Also you can ignore Node.java unless you want to use my Node object.

---
+++ Asst-1/MazeRunner/src/app/MazeRunner.java
@@ -0,0 +1,24 @@
+package app;
+import structures.*;
+import java.util.*;
+import java.util.Queue;
+import java.util.PriorityQueue;
+import java.util.Stack;
+
+public class MazeRunner {
+	 
+	public static void main(String args[]) {
+		
+		//code here...
+		
+		
+		//here are some data structure examples:
+		Stack<String> stack=new Stack<String>();
+		Queue<Integer> queue = new LinkedList<Integer>();
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+	}
+	
+	
+	
+
+}
