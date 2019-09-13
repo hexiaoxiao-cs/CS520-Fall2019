410ea51f258b0bd96c0e23d62d75bbbaf982106c
Xiaoxiao He <xiaoxiao.he@rutgers.edu>
2019-09-13 11:36:14 -0400

    Auto stash before merge of "Arist-Asst1" and "origin/Arist-Asst1"

--- Asst-1/MazeRunner/src/app/MazeRunner.java
+++ Asst-1/MazeRunner/src/app/MazeRunner.java
@@ -12,9 +12,11 @@ public class MazeRunner {
 	public static int dim;
 	public static double prob;
 
-	public static class Coord { // coordinate object. used in fringe.
+	public static class Coord implements Comparable<Coord> { // coordinate object. used in fringe.
 		public int x;
 		public int y;
+		public double weight;
+		public double weight_to_start;
 		public Coord parent;
 		public int f;	//estimated s->n->g. useful for priority queue for A*
 
@@ -22,17 +24,19 @@ public class MazeRunner {
 			this.x = x;
 			this.y = y;
 			this.parent = parent;
+			this.weight=0;
 		}
 
 		public String toString() {
 			return "(" + x + "," + y + ")";
 		}
-		public boolean equals(Object o) {
-			if (!(o instanceof Coord)) { 
-	            return false; 
-	        } 
-			Coord c = (Coord) o; 
-			return this.x==c.x&&this.y==c.y;
+		public int compareTo(Coord another) {
+			if(another.weight==this.weight) {return 0;}
+			if(another.weight>this.weight) {return 1;}
+			else {return -1;}
+		}
+		public boolean equals(Coord c) {
+			return (this.x==c.x)&&(this.y==c.y);
 		}
 	}
 
@@ -163,29 +167,69 @@ public class MazeRunner {
 		
 		
 	}
-	
-	
-	public void Astar() {
-		PriorityQueue<Coord> fringe = new PriorityQueue<Coord>( new Comparator<Coord>() {
-			@Override
-			public int compare(Coord o1, Coord o2) {
-				return o1.f-o2.f;
+	//Astar:
+	//Arguments: 1. which weighting? 0 for euclid, 1 for Manhattan
+	public static void Astar(boolean isManhattan) {
+		PriorityQueue<Coord> open_set = new PriorityQueue<Coord>();
+		open_set.add(new Coord(0,0,null));
+		Coord current = null;
+		Coord goal = null;
+		double curr_weight;
+		double curr_weight_to_start;
+		Coord[][] closed_set=new Coord[grid.dim][grid.dim];
+		while(!open_set.isEmpty()) {
+			current = open_set.poll();//removed from open
+			closed_set[current.x][current.y]=current;//put current to the closed set
+			if(grid.isGoal(current.x, current.y)) {
+				goal=current;
+				break;
 			}
-	    }); //note that fringe will be sorted by Coordinate f values if you assign f-values to coord.
-		
-		
-		
-		
-		
-		
-
+			for (Coord c: grid.getNeighbors(current.x, current.y)) {
+				c.parent = current;//first, set path
+				//check whether in the closed set or (wall, burnt)
+				if(closed_set[c.x][c.y]!=null||grid.isFree(c.x, c.y)==false) {
+					continue;
+				}
+				//open_set.
+				if(isManhattan==true){
+					curr_weight_to_start=current.weight_to_start+Manhattan(current.x,current.y,c.x,c.y);//g(x) Actual distance between new point to the old point
+					
+					curr_weight = curr_weight_to_start+Manhattan(current.x,current.y,grid.dim-1,grid.dim-1);//g(x)+h(x)
+				}
+				else {
+					curr_weight_to_start=current.weight_to_start+Euclid(current.x,current.y,c.x,c.y);//g(x) Actual distance between new point to the old point
+					curr_weight = curr_weight_to_start+Euclid(current.x,current.y,grid.dim-1,grid.dim-1);
+				}
+				System.out.println(curr_weight_to_start);
+				System.out.println(curr_weight+"\n");
+				c.weight=curr_weight;
+				c.weight_to_start=curr_weight_to_start;
+				if(open_set.contains(c)) {
+					if(open_set.removeIf((t)->t.weight>c.weight)==true) {//if in the open set it has a worse node than the one being inserted
+						//sth being deleted
+						open_set.add(c);
+					}
+				}
+				else {
+					//does not have c in open set
+					open_set.add(c);
+				}
+				
+				
+				//fringe.add(c);
+				
+			}
+			
+		}
+		grid.showPath(goal);
+		return;
 	}
 
-	private double Euclid(int x1, int y1, int x2, int y2) { // find euclid distance
+	private static double Euclid(int x1, int y1, int x2, int y2) { // find euclid distance
 		return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
 	}
 
-	private double Manhattan(int x1, int y1, int x2, int y2) { // find manhattan distance
+	private static double Manhattan(int x1, int y1, int x2, int y2) { // find manhattan distance
 		return Math.sqrt(Math.abs(x1 - x2) + Math.abs(y1 - y2));
 	}
 
@@ -194,13 +238,19 @@ public class MazeRunner {
 		dim = 4;
 		prob = 0.01;
 		grid = new Grid(dim, prob); // dim, probability.
-		grid.show();
+		//grid.show();
 
 		//DFS();
 		//grid.show();
 		//grid.clearOccupied();
-		//BiBFS();
+		//BFS();
+		//grid.show();
+		Astar(true);
 		grid.show();
+		grid.clearOccupied();
+		Astar(false);
+		grid.show();
+		grid.clearOccupied();
 
 	}
 
