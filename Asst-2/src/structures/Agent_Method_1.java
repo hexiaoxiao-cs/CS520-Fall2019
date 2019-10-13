f7727b0c9bb38f1f35a1de83df7e8ca27f1b7721
Xiaoxiao He <xiaoxiao.he@rutgers.edu>
2019-10-13 19:20:13 -0400

    Auto stash before merge of "Arist-Asst1" and "origin/Arist-Asst1"

--- Asst-2/src/structures/Agent_Method_1.java
+++ Asst-2/src/structures/Agent_Method_1.java
@@ -29,47 +29,78 @@ public class Agent_Method_1 {
 	//-3-> It cannot be a mine (when the board return 0 set this value
 	
 	//Search: Using BFS to expand the whole minesweeper graph
-	public int[][] getSurrounding(int x, int y){// get surrounding block index
-		int [][] result= new int [8][2];
-		result[0][0]= x-1;
-		result[0][1]= y-1;
-		result[1][0]= x-1;
-		result[1][1]= y;
-		result[2][0]= x-1;
-		result[2][1]= y+1;
-		result[3][0]= x;
-		result[3][1]= y-1;
-		result[4][0]= x;
-		result[4][1]= y+1;
-		result[5][0]= x+1;
-		result[5][1]= y-1;
-		result[6][0]= x+1;
-		result[6][1]= y;
-		result[7][0]= x+1;
-		result[7][1]= y+1;
-		return result;
-	}
+//	public int[][] getSurrounding(int x, int y){// get surrounding block index
+//		int [][] result= new int [8][2];
+//		result[0][0]= x-1;
+//		result[0][1]= y-1;
+//		result[1][0]= x-1;
+//		result[1][1]= y;
+//		result[2][0]= x-1;
+//		result[2][1]= y+1;
+//		result[3][0]= x;
+//		result[3][1]= y-1;
+//		result[4][0]= x;
+//		result[4][1]= y+1;
+//		result[5][0]= x+1;
+//		result[5][1]= y-1;
+//		result[6][0]= x+1;
+//		result[6][1]= y;
+//		result[7][0]= x+1;
+//		result[7][1]= y+1;
+//		return result;
+//	}
+	
+	
 	public void updateKB(Eqn to_insert) {
-		if (to_insert.sum==0) { //Never Insert some Eqn with Sum = 0
-			for (int[] c : to_insert.pts) {
-				safe_nodes.add(c);
-				board.arr[c[0]][c[1]]=board.aSafe;
+		if (to_insert.sum==to_insert.pts.size()||to_insert.sum==0) { //Never Insert some Eqn with Sum = 0
+			if(to_insert.sum==0) {
+				for (int[] c : to_insert.pts) {
+					System.out.println("Safe and Covered added,"+c[0]+","+c[1]);
+					safe_nodes.add(c);
+					
+					board.arr[c[0]][c[1]]=board.aSafeAndCovered;
+				}
+			}
+			else {
+				for (int[] c : to_insert.pts) {
+					//safe_nodes.add(c);
+					System.out.println("Mine and Covered added,"+c[0]+","+c[1]);
+					board.arr[c[0]][c[1]]=board.aMineAndCovered;
+				}
 			}
 			//Update KB
-			for(Eqn c: equations) {
-				for(int[] coord : c.pts) {
-					if(board.arr[coord[0]][coord[1]]==board.aSafe) {
-						c.pts.remove(coord);// delete this entry
+			for(Iterator<Eqn> iter_eqn = equations.iterator();iter_eqn.hasNext();) {
+				Eqn c = iter_eqn.next();
+				for(Iterator<int[]> iter_c =c.pts.iterator();iter_c.hasNext();) {
+					int[] coord = iter_c.next();
+					if(board.arr[coord[0]][coord[1]]==board.aSafeAndCovered) {
+						System.out.println("Removed Safe and Covered "+coord[0]+","+coord[1]);
+						iter_c.remove();// delete this entry
+						
 					}
-					if(board.arr[coord[0]][coord[1]]==board.aMine||board.arr[coord[0]][coord[1]]==board.eMine) { 
-						c.pts.remove(coord);
+					if(board.arr[coord[0]][coord[1]]==board.aMineAndCovered||board.arr[coord[0]][coord[1]]==board.eMine) { 
+						System.out.println("Removed Mine and Covered "+coord[0]+","+coord[1]+",Result Sum"+(c.sum-1));
+						iter_c.remove();
 						c.sum--;
 					}
-					if(c.pts.size()==c.sum) {//only one stuff -> means singleton, And First if statement make sure that sum!=0
-						for(int[] mine: c.pts) {
-							board.arr[mine[0]][mine[1]]=board.aMine;
+					if(c.pts.size()==c.sum||c.sum==0) {//only one stuff -> means singleton, And First if statement make sure that sum!=0
+						if(c.sum==0) {
+							
+							for(int[] mine: c.pts) {
+								board.arr[mine[0]][mine[1]]=board.aSafeAndCovered;
+								System.out.println("Sum=0, Singleton, adding to Safe and Covered"+mine[0]+","+mine[1]);
+								safe_nodes.add(mine);
+							}
+							System.out.println("Equation being removed");
+							iter_eqn.remove();
+							break;
 						}
-						equations.remove(c);
+						for(int[] mine: c.pts) {
+							board.arr[mine[0]][mine[1]]=board.aMineAndCovered;
+							System.out.println("Sum!=0, Singleton, adding to Mine and Covered"+mine[0]+","+mine[1]);
+						}
+						System.out.println("Equation being Removed");
+						iter_eqn.remove();
 						break;
 					}
 				}
@@ -78,21 +109,26 @@ public class Agent_Method_1 {
 		}
 		else {
 			boolean isInserted=false;
-			for (Eqn c : equations) {
+			ArrayList<Eqn> to_add=new ArrayList<Eqn>();
+			//ArrayList<Eqn> to_delete=new ArrayList<Eqn>();
+			System.out.println("Not singleton or trivial answers");
+			for (Iterator<Eqn> iter_eqn= equations.iterator();iter_eqn.hasNext();) {
+				Eqn c = iter_eqn.next();
 				if(c.pts.containsAll(to_insert.pts)) {
 					if(c.pts.size()==to_insert.pts.size()) {//repetitive, rejection
+						System.out.println("Repetitive, Rejection");
 						return;
 					}
 					//non-repetitive, split
 					//indicating to_insert.pts \subseteq c.pts
 					//therefore we will delete c.pts from equations, and delete to_insert.pts stuff and sum from c.pts then insert back to KB
-					equations.remove(c);
+					iter_eqn.remove();
 					c.pts.removeAll(to_insert.pts);
 					c.sum-=to_insert.sum;
-					updateKB(c);
+					to_add.add(c);
 					if(isInserted==false) {
 						isInserted=true;
-						equations.add(to_insert);
+						to_add.add(to_insert);
 					}
 				}
 				else {
@@ -105,18 +141,29 @@ public class Agent_Method_1 {
 						//Then get subset of insert_to and add it back to the set
 						if(isInserted==true) {
 							isInserted=false;
-							equations.remove(to_insert);
+							to_add.remove(to_insert);
 						}
 						to_insert.pts.removeAll(c.pts);
 						to_insert.sum-=c.sum;
 						isInserted=true;
-						equations.add(to_insert);
+						to_add.add(to_insert);
 						
+					}
+					else {
+						isInserted=true;
+						System.out.println("Not Related to any of the equations, inserting");
+						to_add.add(to_insert);
 					}
 				}
 				
 			}
+			
+			if(isInserted==false) {to_add.add(to_insert);}
+			for(Eqn c : to_add) {
+				equations.add(c);
+			}
 		}
+		
 	}
 	
 	public Agent_Method_1(int dim) {
@@ -130,9 +177,23 @@ public class Agent_Method_1 {
 			 return false;
 		 board.arr[x][y]=e.board.arr[x][y];
 		 ArrayList<int[]> add_list= new ArrayList<int[]>();
-		 Eqn a = new Eqn();
+		 int sum = board.arr[x][y]-'0';
+		 for(int[] c : board.getNeighbors(x, y)) {
+
+			 	if(board.arr[c[0]][c[1]]==board.aMineAndCovered) {
+			 		System.out.println("MineAndCovered"+(sum-1));
+			 		sum--;
+			 	}
+				if(board.arr[c[0]][c[1]]==board.aHidden) {
+					System.out.println("Hidden Node,"+c[0]+","+c[1]);
+					add_list.add(c);
+
+			 }
+		 }
+		 System.out.println("Current Eqn originated from"+x+","+y+"with sum"+sum);
+		 Eqn a = new Eqn(add_list,sum);
 		 
-		 updateKB(); 
+		 updateKB(a); 
 		 
 		 return true;
 	}
