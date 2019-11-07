61cfb639aab2799e8bfd373cf158af124fbfe4fd
sarahliang01 <46304579+sarahliang01@users.noreply.github.com>
2019-11-06 22:26:08 -0500

    sorry im busy, but i did #1 formulas. I will do #2 later tomorrow. Also it goes into an infinte loop because i didnt do #2 yet.

--- Asst-3/SearchAndDestroy/src/app/Driver.java
+++ Asst-3/SearchAndDestroy/src/app/Driver.java
@@ -1,14 +1,91 @@
 package app;
 
+import java.util.stream.*;
+
 import structures.*;
 
 public class Driver {
-
+	
+	final static int dim=10;
+	static double[][] belief;	
+	static Map map;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		final int dim=10;
-		Map map=new Map(dim);
+		int numQueries=0;
+		map=new Map(dim);
+		belief=new double[dim][dim];
+		//Initialize:
+		for(int i=0;i<dim;i++) {
+			for(int j=0;j<dim;j++) {
+				belief[i][j]=1.0/(dim*dim);
+			}
+		}
 		map.show();
+		int queriedX=(int)(Math.random()*dim);
+		int queriedY=(int)(Math.random()*dim);
+		while(!map.query(queriedX,queriedY) ){
+			numQueries++;
+			updateBeliefMatrix(queriedX,queriedY);	
+			showBeliefs();
+			//queriedX= ;	<--set next coord to query here
+			//queriedY= ;   <--set next coord to query here (or else it will go in an infinite loop)
+			
+		}
+		System.out.println("Number of Queries="+numQueries);
+		
+		 
+	}
+	
+	private static void updateBeliefMatrix(int x,int y) {
+		updateSingle(x,y);
+		updateOthers(x,y);
+	}
+	
+	private static void updateSingle(int x,int y) {	//failed x and y
+		double b=belief[x][y];
+		double falseNegRate=map.arr[x][y].falseNegProb;
+		belief[x][y]=(b*falseNegRate)/(b*falseNegRate+(1-b));//formula
+	}
+	
+	private static void updateOthers(int x, int y) {	//failed x and y
+		map.getAllCoords().stream().forEach(c->{ 
+			int cx=c[0];
+			int cy=c[1];
+			if (cx!=x&&cy!=y) {
+				double bj=belief[cx][cy];
+				double bi=belief[x][y];
+				double falseNegRate=map.arr[cx][cy].falseNegProb;
+				belief[cx][cy]=bj/(bi*falseNegRate+(1-bi));//formula
+			}
+		});
+	}
+	
+	
+	
+	private static void showBeliefs() { 
+		for (int i=0;i<dim;i++) {
+			System.out.print(i+"\t");
+			for (int j=0;j<dim;j++) {
+				double b=belief[i][j];
+				if (b==0||b==1) { System.out.print("   "+(int)b); continue;}
+				String s=(""+b); 
+				s=s.substring(1,Math.min(s.length(),5));
+				switch (s.length()) {
+					case 2: System.out.print( "  "+s ); break;
+					case 3: System.out.print( " "+s ); break;
+					case 4: System.out.print( s ); break;
+				} 
+			}
+			System.out.println();
+		} 
+		System.out.print("\n\t");
+		for (int i=0;i<dim;i++) {
+			switch((i+"").length()) {
+				case 1: System.out.print("  "+i+" "); break;
+				case 2: System.out.print(" "+i+" "); break;
+				case 3: System.out.print(" "+i); break;
+			}
+		}
+		System.out.println("\n "); 
 	}
 
 }
--- Asst-3/SearchAndDestroy/src/structures/Map.java
+++ Asst-3/SearchAndDestroy/src/structures/Map.java
@@ -9,24 +9,24 @@ import java.util.stream.Collectors;
 
 public class Map {
 
-	public char[][] arr;
+	public Terrain[][] arr;
 	public int dim=50; 
 	public int[] targetCoord=new int[2];
 	
 	public static class Terrain{
-		double falseNegProb;
-		double assignToCellProb;
-		char character;
+		public double falseNegProb;
+		public double assignToCellProb;
+		public char character;
 		public Terrain(double falseNegProb,double assignToCellProb,char character) {
 			this.falseNegProb=falseNegProb;
 			this.assignToCellProb=assignToCellProb;
 			this.character=character;
 		}
 	}
-	public final static Terrain flat=new Terrain(0.1,0.2,' ');	//(0,.2]
-	public final static Terrain hilly=new Terrain(0.3,0.5,'H');	//(.2,.5]
-	public final static Terrain forest=new Terrain(0.7,0.7,'F');	//(.5,.7]
-	public final static Terrain cave=new Terrain(0.9,1,'C');	//(.7,1.0]
+	public final static Terrain flat=new Terrain(0.1,0.2,'_');	//(0,.2]
+	public final static Terrain hilly=new Terrain(0.3,0.5,'^');	//(.2,.5]
+	public final static Terrain forest=new Terrain(0.7,0.7,'*');	//(.5,.7]
+	public final static Terrain cave=new Terrain(0.9,1,'O');	//(.7,1.0]
 	
 	
 	
@@ -35,44 +35,45 @@ public class Map {
 		targetCoord[1]=(int)(Math.random()*dim);
 		//initialize board:
 		this.dim=dim; 
-		arr=new char[dim][dim];
+		arr=new Terrain[dim][dim];
 		for(int i=0;i<dim;i++) {
 			for(int j=0;j<dim;j++) {
 				double rand=((double)(Math.random()*1000)+1)/1000;  
 				if (rand<flat.assignToCellProb) {
-					arr[i][j]= flat.character; 
+					arr[i][j]= flat; 
 				}else if (rand<hilly.assignToCellProb) {
-					arr[i][j]= hilly.character; 
+					arr[i][j]= hilly; 
 				}else if (rand<forest.assignToCellProb) {
-					arr[i][j]= forest.character; 
+					arr[i][j]= forest; 
 				}else {
-					arr[i][j]=cave.character;
+					arr[i][j]=cave;
 				}
 			}
 		} 
 	}
 
-	 
+	public boolean query(int x,int y) {
+		return x==targetCoord[0]&&y==targetCoord[1];
+	} 
 
 	public void show() { 
 		for (int i=0;i<dim;i++) {
 			System.out.print(i+"\t");
 			for (int j=0;j<dim;j++) {
 				if (targetCoord[0]==i&&targetCoord[1]==j)
-					System.out.print( "["+arr[i][j] +"]" );
+					System.out.print( "["+arr[i][j].character +"]" );
 				else
-					System.out.print( " "+arr[i][j] +" " );
+					System.out.print( " "+arr[i][j].character +" " );
 			}
 			System.out.println();
 		} 
 		System.out.print("\n\t");
 		for (int i=0;i<dim;i++) {
-			if ((i+"").length()==1)
-				System.out.print(" "+i+" ");
-			else if ((i+"").length()==2)
-				System.out.print(" "+i);
-			else
-				System.out.print(i);
+			switch((i+"").length()) {
+			case 1: System.out.print(" "+i+" "); break;
+			case 2: System.out.print(" "+i); break;
+			case 3: System.out.print(i); break;
+			}
 		}
 		System.out.println("\n ");
 
