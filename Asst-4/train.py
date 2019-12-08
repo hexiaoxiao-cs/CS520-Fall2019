b19f01ad7013609bea6b21f5ecac5c541c602296
Arist-Mac <xiaoxiao.he@rutgers.edu>
2019-12-07 19:58:33 -0500

    upload

--- Asst-4/train.py
+++ Asst-4/train.py
@@ -19,15 +19,15 @@ def read_images():
     for image in tqdm.tqdm(glob(training_folder+"*.jpg")):
         basename=os.path.basename(image)
         filename=os.path.splitext(basename)[0]
-        x_truth.append(imageio.imread(training_folder+filename+"_bw.jpeg"))
-        y_truth.append(imageio.imread(image))
+        x_truth.append(imageio.imread(training_folder+filename+"_bw.jpeg")/256)
+        y_truth.append(imageio.imread(image)/256)
 
         file_order.append(filename)
     return x_truth, y_truth, file_order
 
 def generate_validate_set(size, prob=0.1):
     validation_list=random.sample(range(size),int(size*prob*2))
-    training_list = [x for x in list(range(validation_list.size)) if x not in validation_list]
+    training_list = [x for x in list(range(len(validation_list))) if x not in validation_list]
     test_list=validation_list[:len(validation_list)//2]
     validation_list = validation_list[len(validation_list) // 2:]
 
@@ -39,7 +39,7 @@ def write_current_configuration(validation_list,test_list,file_order):
     pickle.dump(test_list, open("test_list_" + str(now) + ".p", "wb"))
     pickle.dump(file_order, open("file_order" + str(now) + ".p", "wb"))
 
-def normalize_image(x_image,y_image): #normalize to -0.5,0.5
+def normalize_image(x_image,y_image):
     x_image=x_image/256
     y_image=y_image/256
     return x_image,y_image
@@ -119,8 +119,8 @@ def begin_training(x_data,y_data,x_validation_data,y_validation_data,x_test_data
     pickle.dump(networks,open("networks_dump_"+str(now)+".p", "wb"))
     #try 1-> Linearize and use 4 dense layer each with 3 densely connected layers
 
-x_truth,y_truth,file_order=read_images()
-normalize_image(x_truth,y_truth)
+x_truth=pickle.load(open("x_truth.p"))
+y_truth=pickle.load(open("y_truth.p"))
 training_list,validation_list,test_list=generate_validate_set(len(x_truth))
 x_data,y_data,x_validation_data,y_validation_data,x_test_data,y_test_data=prepare_data(x_truth,y_truth,training_list,validation_list,test_list)
 write_current_configuration(training_list,validation_list,test_list)
