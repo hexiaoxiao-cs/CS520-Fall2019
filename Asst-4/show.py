ba6dc28e73a0390a103abb2b9071fb7d513eee01
Xiaoxiao He <xh172@ilab3.cs.rutgers.edu>
2019-12-10 23:06:22 -0500

    stuff

--- Asst-4/show.py
+++ Asst-4/show.py
@@ -8,13 +8,19 @@ network_num=8
 
 networks=pickle.load(open("networks_dump_"+str(network_num)+".p","rb"))
 
-to_test= imageio.imread("./training/2350507_bw.jpeg")
+to_test= imageio.imread("./training/2350507_bw.jpeg")/256
 
-input=np.dstack((to_test,to_test,to_test))
+
+input=np.dstack((to_test,to_test,to_test)).flatten()
+
+prev=input
 
 for layers in networks:
-    prev=layers.forward(input)
+    prev=layers.forward(prev)
     prev=modules.sigmoid(prev)
 
-prev.reshape((64,64,3))
+prev=prev*256
+print(prev)
+prev=prev.reshape((64,64,3))
+
 imageio.imwrite("test.jpg",prev)
